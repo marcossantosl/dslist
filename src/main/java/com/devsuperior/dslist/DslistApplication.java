9f0bf22d0e2b4763be83f89b2dcce7d09e58ddf1
marcossantosl <marcodossantosluiz111@gmail.com>
2025-01-22 21:16:42 -0300

    proprerties, Game, ORM

--- src/main/java/com/devsuperior/dslist/DslistApplication.java
+++ src/main/java/com/devsuperior/dslist/DslistApplication.java
@@ -1,4 +1,5 @@
-package com.devsuperior.dslist;
+package com.devsuperior.Dslist;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
