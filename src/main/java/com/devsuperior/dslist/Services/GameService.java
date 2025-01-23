8df9bafc0758490fd852942a561f0ee6afa4897f
marcossantosl <marcodossantosluiz111@gmail.com>
2025-01-23 20:13:23 -0300

    Get by ID

--- src/main/java/com/devsuperior/dslist/Services/GameService.java
+++ src/main/java/com/devsuperior/dslist/Services/GameService.java
@@ -4,13 +4,19 @@
 package com.devsuperior.Dslist.Services;
 
 import java.util.List;
+
+import com.devsuperior.Dslist.Dto.GameDTO;
 import com.devsuperior.Dslist.Dto.GameMinDTO;
 import com.devsuperior.Dslist.Entities.Game;
 import com.devsuperior.Dslist.repositories.GameRepository;
+
+
+
 import org.springframework.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service //componente, é necessário definir como componente. Ela é usada para indicar que uma classe é um "serviço" dentro da camada de negócios de uma aplicação.
@@ -19,6 +25,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         var result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();//Cria uma lista do tipo game mindto que em que todo objeto que era x original que era game transforma em new gamemindto passando x como argumento e transforma em uma lista de dto
--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -5,12 +5,14 @@ package com.devsuperior.Dslist.controllers;
 
 import java.util.List;
 
+import com.devsuperior.Dslist.Dto.GameDTO;
 import com.devsuperior.Dslist.Dto.GameMinDTO;
 import com.devsuperior.Dslist.Entities.*;
 import com.devsuperior.Dslist.Services.GameService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,11 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll() {
         List<GameMinDTO> result = gameService.findAll();
+                return result;  
+    }
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
                 return result;  
     }
 }
